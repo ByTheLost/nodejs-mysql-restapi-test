d0bd9e2eaca5aac486598a8d9571077b93500f52
Gary-DevOps <75078672stevenmuriel@gmail.com>
2022-08-16 00:42:33 -0500

    Crud para las quejas con ErrorHandling

--- server/controllers/claims.controller.js
+++ server/controllers/claims.controller.js
@@ -1,31 +1,76 @@
 import { pool } from '../db.js';
 
-export const getClaims = (req, res) => {
-  res.send('Obteniendo Quejas');
+// Buscar todas la quejas
+export const getClaims = async (req, res) => {
+  try {
+    const [ result ] = await pool.query(
+      'SELECT * FROM claims ORDER BY createdAt ASC'
+    );
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
 };
 
-export const getClaim = (req, res) => {
-  res.send('Obteniendo una Queja');
+// Buscar una sola queja
+export const getClaim = async (req, res) => {
+  try {
+    const [ result ] = await pool.query('SELECT * FROM claims WHERE id = ?', [
+      req.params.id
+    ]);
+    if (result.length === 0) {
+      return res.status(404).json({message: 'Queja no encontrada.'});
+    }
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
 };
 
+// Crear una queja
 export const createClaim = async (req, res) => {
-  const { title, description } = req.body;
-  const [result] = await pool.query('INSERT INTO claims(title, description) VALUES (?,?)', [
-    title,
-    description
-  ]);
-  console.log(result);
-  res.json({
-    id: result.insertId,
-    title, 
-    description,
-  });
+  try {
+    const { title, description } = req.body;
+    const [result] = await pool.query('INSERT INTO claims(title, description) VALUES (?,?)', [
+      title,
+      description
+    ]);
+    console.log(result);
+    res.json({
+      id: result.insertId,
+      title, 
+      description,
+    });
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
 };
 
-export const updateClaim = (req, res) => {
-  res.send('Actualizando Queja');
+// Actualizar queja
+export const updateClaim = async (req, res) => {
+  try {
+    const result = await pool.query('UPDATE claims SET ? WHERE id = ?', [
+      req.body, 
+      req.params.id
+    ]);
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
 };
 
-export const deleteClaim = (req, res) => {
-  res.send('Eliminando Queja');
+// Eliminar una queja
+export const deleteClaim = async (req, res) => {
+  try {
+    const [ result ] = await pool.query('DELETE FROM claims WHERE id = ?', [
+      req.params.id
+    ]);
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: 'Queja no encontrada' });
+    };
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
 };
