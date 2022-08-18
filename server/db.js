9f13fdb318430ae9bd5ba0ee656a8b182d946428
GaryDevOps <75078672stevenmuriel@gmail.com>
2022-08-17 19:20:45 -0500

    Cambios

--- server/db.js
+++ server/db.js
@@ -1,9 +1,18 @@
 import { createPool } from 'mysql2/promise';
 
-export const pool = createPool({
+/* export const pool = createPool({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: '1002543180',
   database: 'Claimsdb'  
+}); */
+
+export const pool = createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
+
